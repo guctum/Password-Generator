167865b4ec6052a5e3b5eb7bc652542166bd7139
Greg Uctum <uctumg@mail.gvsu.edu>
2018-08-15 22:39:37 -0400

    Project on Git.

---
+++ Password-Generator/Main.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Password_Generator
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(285, 199);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(378, 199);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Close";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(282, 261);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Your password:";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(418, 261);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(0, 13);
+            this.lblPassword.TabIndex = 3;
+            // 
+            // Main
+            // 
+            this.AcceptButton = this.btnGenerate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnGenerate);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Main";
+            this.Text = "Password Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPassword;
+    }
+}
+
---
+++ Password-Generator/Main.cs
@@ -0,0 +1,141 @@
+﻿/*
+ * Author: Greg Uctum
+ * Template: https://codeshare.co.uk/blog/how-to-create-a-random-password-generator-in-c/
+ * Version: 1.0
+ * Date: 8/15/2018
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using Password_Generator.CodeShare.Library.Passwords;
+
+namespace Password_Generator
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+                bool includeLowercase = true;
+                bool includeUppercase = true;
+                bool includeNumeric = true;
+                bool includeSpecial = true;
+                bool includeSpaces = false;
+                int lengthOfPassword = 17;
+
+                string password = PasswordGenerator.GeneratePassword(includeLowercase, includeUppercase, includeNumeric, includeSpecial, includeSpaces, lengthOfPassword);
+
+                while (!PasswordGenerator.PasswordIsValid(includeLowercase, includeUppercase, includeNumeric, includeSpecial, includeSpaces, password))
+                {
+                    password = PasswordGenerator.GeneratePassword(true, true, true, true, false, lengthOfPassword);
+                }
+
+            lblPassword.Text = password;
+            }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+    namespace CodeShare.Library.Passwords
+    {
+        public static class PasswordGenerator
+        {
+            public static string GeneratePassword(bool includeLowercase, bool includeUppercase, bool includeNumeric, bool includeSpecial, bool includeSpaces, int lengthOfPassword)
+            {
+                const int maximumIdenticalConsecutiveChars = 2;
+                const string lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+                const string uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                const string numericCharacters = "0123456789";
+                const string specialCharacters = @"!#$%&*@\";
+                const string spaceCharacter = " ";
+                const int passwordLengthMin = 8;
+                const int passwordLengthMax = 30;
+
+                if (lengthOfPassword < passwordLengthMin || lengthOfPassword > passwordLengthMax)
+                {
+                    return "Password length must be between 8 and 30.";
+                }
+
+                string characterSet = "";
+
+                if (includeLowercase)
+                {
+                    characterSet += lowercaseCharacters;
+                }
+
+                if (includeUppercase)
+                {
+                    characterSet += uppercaseCharacters;
+                }
+
+                if (includeNumeric)
+                {
+                    characterSet += numericCharacters;
+                }
+
+                if (includeSpecial)
+                {
+                    characterSet += specialCharacters;
+                }
+
+                if (includeSpaces)
+                {
+                    characterSet += spaceCharacter;
+                }
+
+                char[] password = new char[lengthOfPassword];
+                int characterSetLength = characterSet.Length;
+
+                System.Random random = new System.Random();
+                for (int characterPosition = 0; characterPosition < lengthOfPassword; characterPosition++)
+                {
+                    password[characterPosition] = characterSet[random.Next(characterSetLength - 1)];
+
+                    bool moreThanTwoIdenticalInARow =
+                        characterPosition > maximumIdenticalConsecutiveChars
+                        && password[characterPosition] == password[characterPosition - 1]
+                        && password[characterPosition - 1] == password[characterPosition - 2];
+
+                    if (moreThanTwoIdenticalInARow)
+                    {
+                        characterPosition--;
+                    }
+                }
+
+                return string.Join(null, password);
+            }
+
+            public static bool PasswordIsValid(bool includeLowercase, bool includeUppercase, bool includeNumeric, bool includeSpecial, bool includeSpaces, string password)
+            {
+                const string regexLowercase = @"[a-z]";
+                const string regexUppercase = @"[A-Z]";
+                const string regexNumeric = @"[\d]";
+                const string regexSpecial = @"([!#$%&*@\\])+";
+                const string regexSpace = @"([ ])+";
+
+                bool lowerCaseIsValid = !includeLowercase || (includeLowercase && Regex.IsMatch(password, regexLowercase));
+                bool upperCaseIsValid = !includeUppercase || (includeUppercase && Regex.IsMatch(password, regexUppercase));
+                bool numericIsValid = !includeNumeric || (includeNumeric && Regex.IsMatch(password, regexNumeric));
+                bool symbolsAreValid = !includeSpecial || (includeSpecial && Regex.IsMatch(password, regexSpecial));
+                bool spacesAreValid = !includeSpaces || (includeSpaces && Regex.IsMatch(password, regexSpace));
+
+                return lowerCaseIsValid && upperCaseIsValid && numericIsValid && symbolsAreValid && spacesAreValid;
+            }
+        }
+    }
+}
